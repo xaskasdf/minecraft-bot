77f19ca531a8f2ae503f4a73a140d02357b976a4
Samuel Cortes <samuel.cortes@usach.cl>
2023-06-17 05:16:48 -0400

    Minor code optimizations

--- api.js
+++ api.js
@@ -1,33 +1,37 @@
-import fetch from "isomorphic-fetch";
 import dotenv from "dotenv";
 import debug from "debug";
 const { Configuration, OpenAIApi } = require("openai");
 
+// Set up debug loggers
 const log = debug("minecraft-openai.api:log");
 const error = debug("minecraft-openai.api:error");
+
+// Constants
 const STOP_WORD = "//";
 const EOL = "\n";
 
+// Load .env configurations
 dotenv.config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// Check if OPENAI_API_KEY is present
+if (!configuration) {
+  error("ERROR: OPENAI_API_KEY is required.");
+  throw (error, "ERROR: OPENAI_API_KEY is required.");
+}
+
+const openai = new OpenAIApi(configuration);
+
 /**
  * Call the OpenAI API with the previous context and the new user input.
- *
  * @param {string} input The user's input from the Minecraft chat.
  * @param {string} context The previous context to be sent to the OpenAI API
  * @returns {Promise<{ id: string, object: string, created: number, mode: string, choices: Array<{ text: string, index: number, logprobs: any, finish_reason: text }> }>}
  */
 export async function callGPT(input, context) {
-  if (!configuration) {
-    error("ERROR: OPENAI_API_KEY is required.");
-    throw (error, "ERROR: OPENAI_API_KEY is required.");
-  }
-  const openai = new OpenAIApi(configuration);
-
   const request = {
     model: "gpt-4",
     prompt: `${context}${EOL}${STOP_WORD} ${input}${EOL}`,
@@ -47,20 +51,6 @@ export async function callGPT(input, context) {
   });
 
   log(`response ${response}`);
-
-  // const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     Authorization: `Bearer ${openAIkey}`,
-  //   },
-  //   body: JSON.stringify(body),
-  // });
-
-  // if (!response.ok) {
-  //   error("api response failed with statis %s", response.statusText);
-  //   return;
-  // }
 
   return await response.data;
 }
