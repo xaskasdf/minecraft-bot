e17ab24f0d355a6a2f98cfb565f5eb6c2ef9be9f
Samuel Cortes <samuel.cortes@usach.cl>
2023-06-17 05:28:16 -0400

    Minor code optimizations

--- cli.js
+++ cli.js
@@ -2,8 +2,8 @@
  * /usr/bin/env node
  */
 
+import bot from "./bot";
 import debug from "debug";
-import bot from "./bot.js";
 import { program } from "commander";
 
 const log = debug("minecraft-openai.cli:log");
@@ -17,7 +17,7 @@ program.command('start')
   .description('start the bot')
   .option("--host <host>", "hostname of the minecraft server", "localhost")
   .option("--port <port>", "port of the minecraft server", 25565)
-  .option("--username <username>", "username of the bot", "OpenAI")
+  .option("--username <username>", "username of the bot", "GPT")
   .action(async(options) => {
     log("starting bot");
     await bot(options.host, options.port, "GPT").catch(error);
